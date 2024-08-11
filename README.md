5351753720d3db77cfab098834527fae1eafa299
Ed Marquez <72571340+ed-marquez@users.noreply.github.com>
2024-08-11 17:16:52 -0400

    Completed all test cases for non-contract managed tokens

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat ignition deploy ./ignition/modules/Lock.js
 ```
+
+Hedera tokens can have three types of custom fees:
+
+Fixed Fee: A set amount transferred to a fee collector account each time the token is transferred, payable in HBAR or another Hedera token (but not NFTs). Fixed Fees can apply to both fungible and non-fungible tokens.
+
+Fractional Fee: A percentage of the transferred tokens, with optional minimum and maximum limits. The sender or receiver can be designated to pay this fee. Fractional Fees apply only to fungible tokens.
+
+Royalty Fee: A fraction of the value exchanged for an NFT, or a fallback fixed fee if no value is exchanged. Royalty Fees are specific to non-fungible tokens (NFTs).
+
+Each token can have up to 10 custom fees, and fee collectors can be exempted from paying these fees.
--- test/createTokens.test.js
+++ test/createTokens.test.js
@@ -33,11 +33,12 @@ describe("HTS token creation via contract interfaces", function () {
 	// Token setup
 	let ftTokenId, ftTokenInfo, ftTokenAddress, nftTokenId, nftTokenInfo, nftTokenAddress;
 	let callerAccountKeyValue, contractAddressKeyValue, ecdsaKeyValue, ecdsaPvKey, ecdsaPbKey, ed25519KeyValue, ed25519PvKey, ed25519PbKey;
+	let fixedFee, fractionalFee;
 
 	// HTS system contract address and gas limit
 	const htsSystemContractAddress = "0x0000000000000000000000000000000000000167";
 	const myContractAddress = "0x02abfe8f63f7b2a09bb11327533aa7b438f45edf"; // 0.0.4542295
-	const gasLimit = 8000000; // Set your desired gas limit
+	const gasLimit = 6500000; // Set your desired gas limit
 
 	before(async function () {
 		console.log(`- Checking accounts and setting up HTS tokens for test cases...\n`);
@@ -87,205 +88,126 @@ describe("HTS token creation via contract interfaces", function () {
 			ECDSA_secp256k1: "0x",
 			delegatableContractId: ethers.ZeroAddress,
 		};
-	});
 
-	it("1. Should create a fungible HTS token with ECDSA & ED25519 keys via a contract", async function () {
-		// Define TokenKey instances
-		const adminKey = {
-			keyType: 1, // adminKey
-			key: callerAccountKeyValue,
-		};
-		const kycKey = {
-			keyType: 2, // kycKey
-			key: callerAccountKeyValue,
-		};
-		const freezeKey = {
-			keyType: 4, // freezeKey
-			key: callerAccountKeyValue,
-		};
-		const wipeKey = {
-			keyType: 8, // wipeKey
-			key: callerAccountKeyValue,
-		};
-		const supplyKey = {
-			keyType: 16, // supplyKey
-			key: callerAccountKeyValue,
-		};
-		const feeScheduleKey = {
-			keyType: 32, // feeScheduleKey
-			key: callerAccountKeyValue,
-		};
-		const pauseKey = {
-			keyType: 64, // pauseKey
-			key: callerAccountKeyValue,
-		};
-
-		const myToken = {
-			name: "MyToken",
-			symbol: "MTK",
-			treasury: treasurySigner.address,
-			memo: "memo",
-			tokenSupplyType: true,
-			maxSupply: 10,
-			freezeDefault: false,
-			tokenKeys: [adminKey, kycKey, freezeKey, wipeKey, supplyKey, feeScheduleKey, pauseKey],
-			expiry: {
-				second: 0,
-				autoRenewAccount: treasurySigner.address,
-				autoRenewPeriod: 8000000,
-			},
-		};
-		const initialTotalSupply = 10;
-		const decimals = 0;
-
-		// Define the payable amount (in wei)
-		const payableHbarAmount = ethers.parseUnits("40", "ether");
-
-		// Execute the token create
-		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
-		const tokenCreateTx = await treasuryIHederaTokenService.createFungibleToken(myToken, initialTotalSupply, decimals, {
-			value: payableHbarAmount, // Include the payable amount here
-			gasLimit: gasLimit,
-		});
-		const tokenCreateRx = await tokenCreateTx.wait();
-		const txHash = tokenCreateRx.hash;
-		console.log(`\n- Hash for token create transaction: \n${txHash}`);
-	});
-
-	it("2. Should create a fungible HTS token with contract keys via a contract", async function () {
-		// Define TokenKey instances
-		const adminKey = {
-			keyType: 1, // adminKey
-			key: callerAccountKeyValue,
-		};
-		const kycKey = {
-			keyType: 2, // kycKey
-			key: contractAddressKeyValue,
-		};
-		const freezeKey = {
-			keyType: 4, // freezeKey
-			key: contractAddressKeyValue,
-		};
-		const wipeKey = {
-			keyType: 8, // wipeKey
-			key: contractAddressKeyValue,
-		};
-		const supplyKey = {
-			keyType: 16, // supplyKey
-			key: contractAddressKeyValue,
-		};
-		const feeScheduleKey = {
-			keyType: 32, // feeScheduleKey
-			key: contractAddressKeyValue,
-		};
-		const pauseKey = {
-			keyType: 64, // pauseKey
-			key: contractAddressKeyValue,
-		};
-
-		const myToken = {
-			name: "MyToken2",
-			symbol: "MTK2",
-			treasury: treasurySigner.address,
-			memo: "memo",
-			tokenSupplyType: true,
-			maxSupply: 10,
-			freezeDefault: false,
-			tokenKeys: [adminKey, kycKey, freezeKey, wipeKey, supplyKey, feeScheduleKey, pauseKey],
-			expiry: {
-				second: 0,
-				autoRenewAccount: treasurySigner.address,
-				autoRenewPeriod: 8000000,
-			},
-		};
-		const initialTotalSupply = 10;
-		const decimals = 0;
-
-		// Define the payable amount (in wei)
-		const payableHbarAmount = ethers.parseUnits("40", "ether");
-
-		// Execute the token create
-		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
-		const tokenCreateTx = await treasuryIHederaTokenService.createFungibleToken(myToken, initialTotalSupply, decimals, {
-			value: payableHbarAmount, // Include the payable amount here
-			gasLimit: gasLimit,
-		});
-		const tokenCreateRx = await tokenCreateTx.wait();
-		const txHash = tokenCreateRx.hash;
-		console.log(`\n- Hash for token create transaction: \n${txHash}`);
-	});
-
-	it("3. Should create a fungible HTS token with ECDSA keys and custom fees via a contract", async function () {
-		// Define TokenKey instances
-		const adminKey = {
-			keyType: 1, // adminKey
-			key: callerAccountKeyValue,
-		};
-		const kycKey = {
-			keyType: 2, // kycKey
-			key: callerAccountKeyValue,
-		};
-		const freezeKey = {
-			keyType: 4, // freezeKey
-			key: callerAccountKeyValue,
-		};
-		const wipeKey = {
-			keyType: 8, // wipeKey
-			key: callerAccountKeyValue,
-		};
-		const supplyKey = {
-			keyType: 16, // supplyKey
-			key: callerAccountKeyValue,
-		};
-		const feeScheduleKey = {
-			keyType: 32, // feeScheduleKey
-			key: callerAccountKeyValue,
-		};
-		const pauseKey = {
-			keyType: 64, // pauseKey
-			key: callerAccountKeyValue,
-		};
-
-		const myToken = {
-			name: "MyToken3",
-			symbol: "MTK3",
-			treasury: treasurySigner.address,
-			memo: "memo",
-			tokenSupplyType: true,
-			maxSupply: 10,
-			freezeDefault: false,
-			tokenKeys: [adminKey, kycKey, freezeKey, wipeKey, supplyKey, feeScheduleKey, pauseKey],
-			expiry: {
-				second: 0,
-				autoRenewAccount: treasurySigner.address,
-				autoRenewPeriod: 8000000,
-			},
-		};
-		const initialTotalSupply = 10;
-		const decimals = 0;
-
-		// Define the fixed fee
-		const fixedFee = [
+		// Fee schedules
+		fixedFee = [
 			{
-				amount: 100000000, // Amount of fee - 1 HBAR
+				amount: 100000000, // Amount of fee: 1 HBAR
 				tokenId: ethers.ZeroAddress, // Token ID of fee
-				useHbarsForPayment: true, // Denomination of fee
-				useCurrentTokenForPayment: false, // Denomination of fee
+				useHbarsForPayment: true, // Use HBAR for payment
+				useCurrentTokenForPayment: false, // Use the token being transferred for payment
 				feeCollector: treasurySigner.address, // Address of fee collector
 			},
 		];
 
-		// Define the fractional fee
-		const fractionalFee = [
+		fractionalFee = [
 			{
 				numerator: 5, // Numerator of fee
 				denominator: 100, // Denominator of fee
-				minimumAmount: 1, // Minimum amount of fee
+				minimumAmount: 1, // Minimum amount of fee (in token units)
 				maximumAmount: 0, // Maximum amount of fee (0 implies no max)
 				netOfTransfers: true, // Net of transfers
 				feeCollector: treasurySigner.address, // Address of fee collector
 			},
 		];
+
+		royaltyFee = [
+			{
+				numerator: 5, // Numerator of fee
+				denominator: 100, // Denominator of fee
+				// Fallback fee starts
+				amount: 100000000, // Amount of fallback fee: 1 HBAR
+				tokenId: ethers.ZeroAddress, // Token ID of fallback fee
+				useHbarsForPayment: true, // Use HBAR for fallback fee payment
+				// Fallback fee ends
+				feeCollector: treasurySigner.address, // Address of fee collector
+			},
+		];
+	});
+
+	it("1. Should create an immutable fungible HTS token via IHederaTokenService/HederaTokenService that has NO custom fixed/fractional fees and NO keys", async function () {
+		const supply = 100;
+		const myImmutableFungibleToken = {
+			name: "myImmutableFungibleToken",
+			symbol: "MIFT",
+			treasury: treasurySigner.address, // The key for this address must sign the transaction or be the caller
+			memo: "This is an immutable fungible token created via the HTS system contract",
+			tokenSupplyType: true, // true for finite, false for infinite
+			maxSupply: supply,
+			freezeDefault: false, // true to freeze by default, false to not freeze by default
+			tokenKeys: [], // No keys. The token is immutable
+			expiry: {
+				second: 0,
+				autoRenewAccount: treasurySigner.address,
+				autoRenewPeriod: 8000000,
+			},
+		};
+		const initialTotalSupply = supply;
+		const decimals = 0;
+
+		// Define the payable amount (in wei)
+		const payableHbarAmount = ethers.parseUnits("40", "ether");
+
+		// Execute the token create
+		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
+		const tokenCreateTx = await treasuryIHederaTokenService.createFungibleToken(myImmutableFungibleToken, initialTotalSupply, decimals, {
+			value: payableHbarAmount, // Include the payable amount here
+			gasLimit: gasLimit,
+		});
+		const tokenCreateRx = await tokenCreateTx.wait();
+		const txHash = tokenCreateRx.hash;
+		console.log(`\n- Hash for token create transaction: \n${txHash}`);
+		console.log(`- See: https://hashscan.io/${network}/transaction/${txHash}`);
+	});
+
+	it("2. Should create a fungible HTS token via IHederaTokenService/HederaTokenService that has custom fixed/fractional fees and ECDSA/ED25519/contract address keys", async function () {
+		// Define TokenKey instances
+		const adminKey = {
+			keyType: 1, // adminKey
+			key: callerAccountKeyValue, // This key must sign the transaction or be the caller
+		};
+		const kycKey = {
+			keyType: 2, // kycKey
+			key: ed25519KeyValue,
+		};
+		const freezeKey = {
+			keyType: 4, // freezeKey
+			key: ecdsaKeyValue,
+		};
+		const wipeKey = {
+			keyType: 8, // wipeKey
+			key: ecdsaKeyValue,
+		};
+		const supplyKey = {
+			keyType: 16, // supplyKey
+			key: callerAccountKeyValue,
+		};
+		const feeScheduleKey = {
+			keyType: 32, // feeScheduleKey
+			key: contractAddressKeyValue,
+		};
+		const pauseKey = {
+			keyType: 64, // pauseKey
+			key: contractAddressKeyValue,
+		};
+
+		const MyFungibleTokenWithKeysAndFees = {
+			name: "MyFungibleTokenWithKeysAndFees",
+			symbol: "MFTWKAF",
+			treasury: treasurySigner.address, // The key for this address must sign the transaction or be the caller
+			memo: "This is a fungible token with keys and fees created via the HTS system contract",
+			tokenSupplyType: true, // true for finite, false for infinite
+			maxSupply: 100,
+			freezeDefault: false, // true to freeze by default, false to not freeze by default
+			tokenKeys: [adminKey, kycKey, freezeKey, wipeKey, supplyKey, feeScheduleKey, pauseKey],
+			expiry: {
+				second: 0,
+				autoRenewAccount: treasurySigner.address,
+				autoRenewPeriod: 8000000,
+			},
+		};
+		const initialTotalSupply = 10;
+		const decimals = 0;
 
 		// Define the payable amount (in wei)
 		const payableHbarAmount = ethers.parseUnits("40", "ether");
@@ -293,7 +215,7 @@ describe("HTS token creation via contract interfaces", function () {
 		// Execute the token create
 		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
 		const tokenCreateTx = await treasuryIHederaTokenService.createFungibleTokenWithCustomFees(
-			myToken,
+			MyFungibleTokenWithKeysAndFees,
 			initialTotalSupply,
 			decimals,
 			fixedFee,
@@ -306,5 +228,105 @@ describe("HTS token creation via contract interfaces", function () {
 		const tokenCreateRx = await tokenCreateTx.wait();
 		const txHash = tokenCreateRx.hash;
 		console.log(`\n- Hash for token create transaction: \n${txHash}`);
+		console.log(`- See: https://hashscan.io/${network}/transaction/${txHash}`);
+	});
+
+	it("3. Should create an non-fungible HTS token via IHederaTokenService/HederaTokenService that has NO custom fixed/fractional fees and NO keys (other than supply key to mint/burn NFTs)", async function () {
+		const supplyKey = {
+			keyType: 16, // supplyKey
+			key: callerAccountKeyValue,
+		};
+
+		const MyImmutableNonFungibleToken = {
+			name: "MyImmutableNonFungibleToken",
+			symbol: "MINFT",
+			treasury: treasurySigner.address, // The key for this address must sign the transaction or be the caller
+			memo: "This is a non-fungible token created via the HTS system contract",
+			tokenSupplyType: true, // true for finite, false for infinite
+			maxSupply: 100,
+			tokenKeys: [supplyKey], // No keys. The token is immutable
+			freezeDefault: false, // true to freeze by default, false to not freeze by default
+			expiry: {
+				second: 0,
+				autoRenewAccount: treasurySigner.address,
+				autoRenewPeriod: 8000000,
+			},
+		};
+
+		// Define the payable amount (in wei)
+		const payableHbarAmount = ethers.parseUnits("40", "ether");
+
+		// Execute the token create
+		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
+		const tokenCreateTx = await treasuryIHederaTokenService.createNonFungibleToken(MyImmutableNonFungibleToken, {
+			value: payableHbarAmount, // Include the payable amount here
+			gasLimit: gasLimit,
+		});
+		const tokenCreateRx = await tokenCreateTx.wait();
+		const txHash = tokenCreateRx.hash;
+		console.log(`\n- Hash for token create transaction: \n${txHash}`);
+		console.log(`- See: https://hashscan.io/${network}/transaction/${txHash}`);
+	});
+
+	it("4. Should create a non-fungible HTS token via IHederaTokenService/HederaTokenService that has custom fixed/fractional fees and ECDSA/ED25519/contract address keys", async function () {
+		// Define TokenKey instances
+		const adminKey = {
+			keyType: 1, // adminKey
+			key: callerAccountKeyValue, // This key must sign the transaction or be the caller
+		};
+		const kycKey = {
+			keyType: 2, // kycKey
+			key: ed25519KeyValue,
+		};
+		const freezeKey = {
+			keyType: 4, // freezeKey
+			key: ecdsaKeyValue,
+		};
+		const wipeKey = {
+			keyType: 8, // wipeKey
+			key: ecdsaKeyValue,
+		};
+		const supplyKey = {
+			keyType: 16, // supplyKey
+			key: callerAccountKeyValue,
+		};
+		const feeScheduleKey = {
+			keyType: 32, // feeScheduleKey
+			key: contractAddressKeyValue,
+		};
+		const pauseKey = {
+			keyType: 64, // pauseKey
+			key: contractAddressKeyValue,
+		};
+
+		const MyNonFungibleToken = {
+			name: "MyNonFungibleTokenWithKeysAndFees",
+			symbol: "MNFTWKAF",
+			treasury: treasurySigner.address, // The key for this address must sign the transaction or be the caller
+			memo: "This is a non-fungible token with keys and fees created via the HTS system contract",
+			tokenSupplyType: true, // true for finite, false for infinite
+			maxSupply: 100,
+			tokenKeys: [adminKey, kycKey, freezeKey, wipeKey, supplyKey, feeScheduleKey, pauseKey], // No keys. The token is immutable
+			freezeDefault: false, // true to freeze by default, false to not freeze by default
+			expiry: {
+				second: 0,
+				autoRenewAccount: treasurySigner.address,
+				autoRenewPeriod: 8000000,
+			},
+		};
+
+		// Define the payable amount (in wei)
+		const payableHbarAmount = ethers.parseUnits("40", "ether");
+
+		// Execute the token create
+		const treasuryIHederaTokenService = await ethers.getContractAt(IHederaTokenServiceABI, htsSystemContractAddress, treasurySigner);
+		const tokenCreateTx = await treasuryIHederaTokenService.createNonFungibleTokenWithCustomFees(MyNonFungibleToken, fixedFee, royaltyFee, {
+			value: payableHbarAmount, // Include the payable amount here
+			gasLimit: gasLimit,
+		});
+		const tokenCreateRx = await tokenCreateTx.wait();
+		const txHash = tokenCreateRx.hash;
+		console.log(`\n- Hash for token create transaction: \n${txHash}`);
+		console.log(`- See: https://hashscan.io/${network}/transaction/${txHash}`);
 	});
 });
